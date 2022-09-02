20fe959c32f5f1baae061df265ec64e2586a54b2
Tanner Swett <tannerswett@gmail.com>
2022-09-01 21:54:39 -0400

    Add a feature for evaluating files from the command line

--- symex/__main__.py
+++ symex/__main__.py
@@ -1,1 +1,11 @@
 from . import *
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', nargs='?')
+
+args = parser.parse_args()
+
+if args.filename:
+    print(eval_file(args.filename))
