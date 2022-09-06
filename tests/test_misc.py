e89ad076124c5e30f202e02d57c95bf216831edc
Tanner Swett <tannerswett@gmail.com>
2022-09-06 19:33:53 -0400

    Unify the interpreter tests

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -10,7 +10,11 @@
 #
 # Copyright 2022 by Tanner Swett.
 
+import pytest
+
 from symex import SAtom, SList, Symex
+from symex.interpreters import Interpreter
+from symex.interpreters.machine import Machine
 from symex.interpreters.simple import Simple
 
 def test_can_parse_an_atom() -> None:
@@ -21,48 +25,86 @@ def test_can_parse_a_list() -> None:
     result = Symex.parse('(one two)')
     assert result == SList([SAtom('one'), SAtom('two')])
 
-def test_empty_where() -> None:
-    input = Symex.parse('(Where :test)')
-    result = Simple().eval(input)
+@pytest.mark.parametrize('interpreter', [Simple(), Machine()])
+class TestInterpreters:
+    def test_can_evaluate_quote(self, interpreter: Interpreter) -> None:
+        input = Symex.parse('(Quote test)')
+        result = interpreter.eval(input)
 
-    assert result == SAtom(':test')
+        assert result == SAtom('test')
 
-def test_simple_where() -> None:
-    input = Symex.parse('(Where color (color :blue))')
-    result = Simple().eval(input)
+    def test_can_evaluate_tail(self, interpreter: Interpreter) -> None:
+        input = Symex.parse('(Tail (Quote (test)))')
+        result = interpreter.eval(input)
 
-    assert result == SAtom(':blue')
+        assert result == SList([])
 
-def test_nested_where() -> None:
-    input = Symex.parse('''
-        (Where (Where color
-                      (flavor :raspberry))
-               (color :blue))
-    ''')
-    result = Simple().eval(input)
+    def test_can_evaluate_data_atom(self, interpreter: Interpreter) -> None:
+        input = SAtom(':test')
+        result = interpreter.eval(input)
 
-    assert result == SAtom(':blue')
+        assert result == SAtom(':test')
 
-def test_hiding_where() -> None:
-    input = Symex.parse('''
-        (Where (Where color
-                      (color :yellow))
-               (color :blue))
-    ''')
-    result = Simple().eval(input)
+    def test_empty_where(self, interpreter: Interpreter) -> None:
+        input = Symex.parse('(Where :test)')
+        result = interpreter.eval(input)
 
-    assert result == SAtom(':yellow')
+        assert result == SAtom(':test')
 
-def test_laugh() -> None:
-    program = '''
-        (Where (Laugh (List :one :two :three :four :five))
-            (Laugh (Function Laugh (list)
-                (Cond ((= list (List))
-                        (List))
-                    (:true
-                        (Cons :ha (Laugh (Tail list))))))))
-    '''
+    def test_simple_where(self, interpreter: Interpreter) -> None:
+        input = Symex.parse('(Where color (color :blue))')
+        result = interpreter.eval(input)
 
-    output = Simple().rep(program)
+        assert result == SAtom(':blue')
 
-    assert output == '(:ha :ha :ha :ha :ha)'
+    def test_expression_where(self, interpreter: Interpreter) -> None:
+        input = Symex.parse('(Where list (list (Head (Quote (test)))))')
+        result = interpreter.eval(input)
+
+        assert result == SAtom('test')
+
+    def test_nested_where(self, interpreter: Interpreter) -> None:
+        input = Symex.parse('''
+            (Where (Where color
+                        (flavor :raspberry))
+                (color :blue))
+        ''')
+        result = interpreter.eval(input)
+
+        assert result == SAtom(':blue')
+
+    def test_hiding_where(self, interpreter: Interpreter) -> None:
+        input = Symex.parse('''
+            (Where (Where color
+                        (color :yellow))
+                (color :blue))
+        ''')
+        result = interpreter.eval(input)
+
+        assert result == SAtom(':yellow')
+
+    def test_simple_function(self, interpreter: Interpreter) -> None:
+        input = Symex.parse('((Function Test (x f) (f x)) (List :hello) Head)')
+        result = interpreter.eval(input)
+
+        assert result == SAtom(':hello')
+
+    def test_simple_cond(self, interpreter: Interpreter) -> None:
+        input = Symex.parse('(Cond (:true (Quote test)))')
+        result = interpreter.eval(input)
+
+        assert result == SAtom('test')
+
+    def test_laugh(self, interpreter: Interpreter) -> None:
+        input = Symex.parse('''
+            (Where (Laugh (List :one :two :three :four :five))
+                (Laugh (Function Laugh (list)
+                    (Cond ((= list (List))
+                            (List))
+                        (:true
+                            (Cons :ha (Laugh (Tail list))))))))
+        ''')
+
+        result = interpreter.eval(input)
+
+        assert result == Symex.parse('(:ha :ha :ha :ha :ha)')
