dee4adac9719a087bc1c2fb61600c54ec3b9b1f3
Tanner Swett <tannerswett@gmail.com>
2022-09-07 19:26:11 -0400

    Add missing license statement to __main__

--- symex/__main__.py
+++ symex/__main__.py
@@ -1,3 +1,15 @@
+# This file is part of PySymex.
+#
+# PySymex is free software: you can redistribute it and/or modify it under the
+# terms of version 3 of the GNU General Public License as published by the Free
+# Software Foundation.
+#
+# PySymex is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# Copyright 2022 by Tanner Swett.
+
 from . import *
 
 import argparse
